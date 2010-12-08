a0aee59b1fd1b7c171d661fff36cbe94e5e75f48
Nathan Sutton <nate@zencoder.com>
2010-12-08 10:07:56 -0600

    Cleaning up whitespace

--- test/inaccessible_attributes_test.rb
+++ test/inaccessible_attributes_test.rb
@@ -4,7 +4,7 @@ require 'rubygems'
 
 
 class InaccessibleAttributesTest < Test::Unit::TestCase
-  
+
   class InaccessibleRecord < ActiveRecord::Base
     attr_accessor :name
 
@@ -21,36 +21,36 @@ class InaccessibleAttributesTest < Test::Unit::TestCase
   def test_attr_protected_should_be_unusable_by_subclasses
     InaccessibleRecord.send(:attr_protected, :name)
   end
-  
+
   def test_should_not_allow_any_attributes_to_be_mass_assigned_by_default
     accessible_attributes = mass_assign_attributes_to_inaccessible_record(:name => 'Batman')
-    
+
     assert !accessible_attributes.include?('Batman')
   end
-  
+
   def test_attributes_should_be_mass_assignable_after_being_specified_with_attr_accessible
     InaccessibleRecord.attr_accessible :name
     accessible_attributes = mass_assign_attributes_to_inaccessible_record(:name => 'Batman')
-    
+
     assert accessible_attributes.include?('Batman')
   ensure
     InaccessibleRecord.accessible_attributes.delete(:name)
   end
-  
+
   def test_should_log_additional_message_when_removed_from_mass_assignment
     setup_testable_logger
     mass_assign_attributes_to_inaccessible_record(:name => 'Batman')
-    
+
     assert_match /Batman/, log.string
   ensure
     revert_to_original_logger
   end
-  
+
   def test_should_not_log_additional_message_when_not_removed_from_mass_assignment
     setup_testable_logger
     InaccessibleRecord.attr_accessible :name
     mass_assign_attributes_to_inaccessible_record(:name => 'Batman')
-  
+
     assert_no_match /Batman/, log.string
   ensure
     revert_to_original_logger
@@ -62,15 +62,15 @@ class InaccessibleAttributesTest < Test::Unit::TestCase
       inaccessible_record = InaccessibleRecord.new
       inaccessible_record.mass_assign(attributes)
     end
-  
+
     def setup_testable_logger
       @original_logger = ActiveRecord::Base.logger
       log = StringIO.new
       ActiveRecord::Base.logger = Logger.new(log)
     end
-    
+
     def revert_to_original_logger
       ActiveRecord::Base.logger = @original_logger
     end
-    
+
 end
